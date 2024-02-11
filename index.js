cabfc820e8cffb967c954eddc9eeb761bb422244
Daniel Ribeiro Skroski <daniel@duplod.com.br>
2024-02-11 08:03:40 -0300

    Codigo para conectar Whatsapp

---
+++ index.js
@@ -0,0 +1,27 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
+const client = new Client({
+    authStrategy: new LocalAuth({
+        dataPath: 'yourFolderName'
+    })
+});
+ 
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', async (message) => {
+	if (message.body === '!ping') {
+		await client.sendMessage(message.from, 'pong');
+	}
+});
+ 
+
+client.initialize();
